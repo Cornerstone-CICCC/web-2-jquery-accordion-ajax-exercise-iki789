f8ad3c4c58b078414fa3065bae574a4f9ff8526f
Ikhlak <ikhlak789@gmail.com>
2024-11-27 15:50:03 -0800

    resolved exercise

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-$(function() {
+$(function () {
   // your code here
-})
+
+  $(".accordion-header").on("click", function (e) {
+    if ($(e.target).next(".accordion-content").is(":hidden") === false) return;
+    $(".accordion-content").slideUp();
+
+    $(e.target).next(".accordion-content").slideDown();
+  });
+
+  $(".todos")
+    .children("button")
+    .on("click", function () {
+      $.ajax({
+        url: "https://dummyjson.com/todos",
+      }).then((res) => {
+        const { todos } = res;
+        const container = $(".todos").find("ul");
+        $.each(todos, function (i, todo) {
+          container.append(`<li>${todo.todo}</li>`);
+        });
+      });
+    });
+});
